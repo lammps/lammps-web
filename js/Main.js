2e348f8d3dc2ff016c2bbf1e35d598dc033365d3
rbberger <rbberger@users.noreply.github.com>
2021-04-16 22:21:31 +0000

    deploy: 89c956101454afda22d27263d0dbb80a706be697

--- js/Main.js
+++ js/Main.js
@@ -1,96 +1,177 @@
 var buttonUpdate, buttonSave, buttonHide
 function createGUI() {
-	// Build GUI
-	var toolbar = document.getElementById( 'toolbar' );
-	buttonUpdate = document.createElement( 'button' );
-	buttonUpdate.className = 'button';
-	buttonUpdate.disabled = true
-	buttonUpdate.appendChild( document.createTextNode( 'Loading ...' ) );
-	buttonUpdate.addEventListener( 'click', function ( event ) {
-		var value = editor.getValue();
-		currentScript = value
-		runScript()
-	}, false );
-	toolbar.appendChild( buttonUpdate );
+    // Build GUI
+    var toolbar = document.getElementById( 'toolbar' );
+    buttonUpdate = document.createElement( 'button' );
+    buttonUpdate.className = 'button';
+    buttonUpdate.disabled = true
+    buttonUpdate.appendChild( document.createTextNode( 'Loading ...' ) );
+    buttonUpdate.addEventListener( 'click', function ( event ) {
+        var value = editor.getValue();
+        currentScript = value;
+        runScript();
+        $('#inputline').focus();
+    }, false );
+    toolbar.appendChild( buttonUpdate );
 
-	// buttonSave = document.createElement( 'button' );
-	// buttonSave.className = 'button';
-	// buttonSave.textContent = 'save';
-	// buttonSave.addEventListener( 'click', function ( event ) {
-	// 	save();
-	// }, false );
-	// toolbar.appendChild( buttonSave );
+    buttonAnimate = document.createElement( 'button' );
+    buttonAnimate.className = 'button';
+    buttonAnimate.disabled = true
+    buttonAnimate.appendChild( document.createTextNode( 'Run & Animate' ) );
+    buttonAnimate.addEventListener( 'click', function ( event ) {
+        var value = editor.getValue();
+        currentScript = value;
+        animateScript();
+        $('#inputline').focus();
+    }, false );
+    toolbar.appendChild( buttonAnimate );
 
-	buttonHide = document.createElement( 'button' );
-	buttonHide.className = 'button';
-	buttonHide.textContent = 'hide code';
-	buttonHide.addEventListener( 'click', toggleEditorVisible, false );
-	toolbar.appendChild( buttonHide );
+    buttonClear = document.createElement( 'button' );
+    buttonClear.className = 'button';
+    buttonClear.appendChild( document.createTextNode( 'Clear Output' ) );
+    buttonClear.addEventListener( 'click', function ( event ) {
+        $('#myconsoleoutput').empty();
+        $('#inputline').focus();
+    }, false );
+    toolbar.appendChild( buttonClear );
+
+    // buttonSave = document.createElement( 'button' );
+    // buttonSave.className = 'button';
+    // buttonSave.textContent = 'save';
+    // buttonSave.addEventListener( 'click', function ( event ) {
+    //     save();
+    // }, false );
+    // toolbar.appendChild( buttonSave );
+
+    buttonHide = document.createElement( 'button' );
+    buttonHide.className = 'button';
+    buttonHide.textContent = 'hide code';
+    buttonHide.addEventListener( 'click', toggleEditorVisible, false );
+    toolbar.appendChild( buttonHide );
+
+    
+    var inputline = document.getElementById("inputline");
+    inputline.addEventListener("keyup", ({key}) => {
+        if (key === "Enter") {
+            currentScript = inputline.value
+            runInputCommands()
+            inputline.value = '';
+        }
+    })
 }
 
 function save() {
-	documents[ 0 ].code = codemirror.getValue();
+    documents[ 0 ].code = codemirror.getValue();
 
-	localStorage.codeeditor = JSON.stringify( documents );
+    localStorage.codeeditor = JSON.stringify( documents );
 
-	var blob = new Blob( [ codemirror.getValue() ], { type: documents[ 0 ].filetype } );
-	var objectURL = URL.createObjectURL( blob );
+    var blob = new Blob( [ codemirror.getValue() ], { type: documents[ 0 ].filetype } );
+    var objectURL = URL.createObjectURL( blob );
 
-	buttonDownload.href = objectURL;
+    buttonDownload.href = objectURL;
 
-	var date = new Date();
-	buttonDownload.download = documents[ 0 ].filename;
+    var date = new Date();
+    buttonDownload.download = documents[ 0 ].filename;
 
-	// buttonSave.style.display = 'none';
-	buttonDownload.style.display = '';
+    // buttonSave.style.display = 'none';
+    buttonDownload.style.display = '';
 }
 
 function toggleEditorVisible() {
-	var code = document.getElementById( 'editor' );
-	if ( code.style.display === '' ) {
-		buttonHide.textContent = 'show code';
-		code.style.display = 'none';
-		buttonUpdate.style.display = 'none';
-		buttonSave.style.display = 'none';
-	} else {
-		buttonHide.textContent = 'hide code';
-		code.style.display = '';
-		buttonUpdate.style.display = '';
-		buttonSave.style.display = '';
-	}
+    var code = document.getElementById( 'ide' );
+    var glwindow = document.getElementById("webglwindow")
+    if ( code.style.display === '' ) {
+        buttonHide.textContent = 'show code';
+        code.style.display = 'none';
+        buttonUpdate.style.display = 'none';
+        buttonAnimate.style.display = 'none';
+        buttonClear.style.display = 'none';
+        //buttonSave.style.display = 'none';
+        glwindow.style.height = (window.innerHeight  - 8) + "px";
+    } else {
+        buttonHide.textContent = 'hide code';
+        code.style.display = '';
+        buttonUpdate.style.display = '';
+        buttonAnimate.style.display = '';
+        buttonClear.style.display = '';
+        //buttonSave.style.display = '';
+        glwindow.style.height = '';
+    }
+    onWindowResize();
+}
+
+function initVSplit() {
+  var dragY = 0;
+  var deltaY = 0;
+
+  element = document.getElementById("vseparator");
+  element.onmousedown = dragMouseDown;
+
+  function dragMouseDown( e )
+  {
+    dragY = e.clientY;
+    document.onmousemove = onMouseMove;
+    document.onmouseup = () => { document.onmousemove = document.onmouseup = null; }
+  }
+
+  function onMouseMove( e )
+  {
+    const currentY = e.clientY;
+
+    deltaY = currentY - dragY;
+
+    const offsetTop = element.offsetTop;
+
+    const first = document.getElementById("ide");
+    const second = document.getElementById("webglwindow");
+    let firstHeight = first.offsetHeight;
+    let secondHeight = second.offsetHeight;
+    element.style.top = offsetTop + deltaY + "px";
+    firstHeight += deltaY;
+    secondHeight -= deltaY;
+    dragY = currentY;
+    first.style.height = firstHeight + "px";
+    second.style.height = secondHeight + "px";
+    onWindowResize();
+  }
 }
 
 var editor = CodeMirror.fromTextArea(document.getElementById("code"), {
-	lineNumbers: true,
-	styleActiveLine: true,
-	matchBrackets: true,
-	viewportMargin: Infinity,
-	extraKeys: {"Ctrl-Space": "autocomplete"}
+    lineNumbers: true,
+    styleActiveLine: true,
+    matchBrackets: true,
+    viewportMargin: Infinity,
+    extraKeys: {"Ctrl-Space": "autocomplete"}
 });
 
 editor.setOption("theme", "monokai");
 
-createGUI()
+createGUI();
+initVSplit();
 
 document.addEventListener( 'keydown', function ( event ) {
-	if ( event.keyCode === 83 && ( event.ctrlKey === true || event.metaKey === true ) ) {
-		event.preventDefault();
-		save();
-	}
+    if ( event.keyCode === 83 && ( event.ctrlKey === true || event.metaKey === true ) ) {
+        event.preventDefault();
+        save();
+    }
 
-	if ( event.keyCode === 82 && ( event.ctrlKey === true || event.metaKey === true ) ) {
-		event.preventDefault();
-		var value = editor.getValue();
-		currentScript = value
-		runScript()
-	}
+    if ( event.keyCode === 82 && ( event.ctrlKey === true || event.metaKey === true ) ) {
+        event.preventDefault();
+        var value = editor.getValue();
+        currentScript = value
+        runScript()
+    }
 }, false );
 
 Module["onRuntimeInitialized"] = function() {
-	var callbackPointer = addFunction(synchronizeLAMMPS, 'v')
-	Atomify.setCallback(callbackPointer)
-	
-	buttonUpdate.disabled = false
-	buttonUpdate.innerHTML = "run lammps script"
+    var callbackPointer = addFunction(synchronizeLAMMPS, 'v')
+    Atomify.setCallback(callbackPointer)
+    
+    buttonUpdate.disabled = false
+    buttonAnimate.disabled = false
+    buttonUpdate.innerHTML = "run lammps script"
+    initLAMMPS();
+    $('#inputline').focus();
 }
+
 initGL();
--- js/gl.js
+++ js/gl.js
@@ -15,25 +15,25 @@ function reallocate() {
 }
 
 function glWindowWidth() {
-	return $('#webglwindow').width();
+    return $('#webglwindow').width();
 }
 
 function glWindowHeight() {
-	return $('#webglwindow').height();
+    return $('#webglwindow').height();
 }
 
 function initGL() {
-	var webglwindow = document.getElementById("webglwindow");
-	container = document.createElement( 'div' );
-	webglwindow.appendChild( container );
-	
-	scene = new THREE.Scene();
-	
-	camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 1000 );
+    var webglwindow = document.getElementById("webglwindow");
+    container = document.createElement( 'div' );
+    webglwindow.appendChild( container );
+    
+    scene = new THREE.Scene();
+    
+    camera = new THREE.PerspectiveCamera( 60, glWindowWidth() / glWindowHeight(), 1, 1000 );
     camera.position.z = 5;
     camera.position.y = 5;
     camera.position.x = 5;
-    controls = new THREE.OrbitControls( camera );
+    controls = new THREE.OrbitControls( camera, webglwindow );
     controls.rotateSpeed = 1.0;
     controls.zoomSpeed = 1.2;
     controls.panSpeed = 0.8;
@@ -43,7 +43,7 @@ function initGL() {
     controls.keys = [ 65, 83, 68 ];
     controls.addEventListener( 'change', render );
 
-	geometry = new THREE.BufferGeometry();
+    geometry = new THREE.BufferGeometry();
     geometry.addAttribute( 'position', new THREE.BufferAttribute( position, 3 ) );
     geometry.addAttribute( 'vertexId', new THREE.BufferAttribute( vertexId, 1 ) );
     geometry.addAttribute( 'radius', new THREE.BufferAttribute( radii, 1 ) );
@@ -56,44 +56,45 @@ function initGL() {
         vertexShader: document.getElementById( 'vertexShader' ).textContent,
         fragmentShader: document.getElementById( 'fragmentShader' ).textContent
     } );
-	
-	mesh = new THREE.Mesh( geometry, material );
-	scene.add(mesh);
+    
+    mesh = new THREE.Mesh( geometry, material );
+    scene.add(mesh);
 
-	renderer = new THREE.WebGLRenderer({ alpha: true });
-	scene.background = new THREE.Color( 0x000000 );
-	renderer.setClearColor( 0x000000, 0);
-	renderer.setPixelRatio( window.devicePixelRatio );
-	// renderer.setSize( window.innerWidth, window.innerHeight );
-	renderer.setSize( glWindowWidth(), glWindowHeight() );
-	container.appendChild( renderer.domElement );
-	onWindowResize()
-	window.addEventListener( 'resize', onWindowResize, false );
+    renderer = new THREE.WebGLRenderer({ alpha: true });
+    scene.background = new THREE.Color( 0x000000 );
+    renderer.setClearColor( 0x000000, 0);
+    renderer.setPixelRatio( window.devicePixelRatio );
+    // renderer.setSize( window.innerWidth, window.innerHeight );
+    renderer.setSize( glWindowWidth(), glWindowHeight() );
+    container.appendChild( renderer.domElement );
+    onWindowResize()
+    window.addEventListener( 'resize', onWindowResize );
 }
 
 function onWindowResize() {
-	camera.aspect = glWindowWidth() / glWindowHeight()
-	camera.updateProjectionMatrix();
-	renderer.setSize( glWindowWidth(), glWindowHeight() );
-	console.log("GL window size: ", glWindowWidth(), ", ", glWindowHeight())
+    camera.aspect = glWindowWidth() / glWindowHeight()
+    camera.updateProjectionMatrix();
+    renderer.setSize( glWindowWidth(), glWindowHeight() );
+    console.log("GL window size: ", glWindowWidth(), ", ", glWindowHeight())
+    render();
 }
 
 function updateVertices() {
-	var systemSizeHalfVec = systemSizeHalf();
-	var systemSizeVec = systemSize();
-	if(maxNumAtoms !== Atomify.numberOfAtoms()) {
-		maxNumAtoms = Atomify.numberOfAtoms()
-		console.log("Reallocating memory to support ", maxNumAtoms, " atoms.")
-		reallocate()
-	}
+    var systemSizeHalfVec = systemSizeHalf();
+    var systemSizeVec = systemSize();
+    if(maxNumAtoms !== Atomify.numberOfAtoms()) {
+        maxNumAtoms = Atomify.numberOfAtoms()
+        console.log("Reallocating memory to support ", maxNumAtoms, " atoms.")
+        reallocate()
+    }
 
-	for ( i = 0; i < Atomify.numberOfAtoms(); i ++ ) {
-		var x = getValue(Atomify.positions() + 24*i, 'double') - systemSizeHalfVec.x;
-		var y = getValue(Atomify.positions() + 24*i + 8, 'double') - systemSizeHalfVec.y;
-		var z = getValue(Atomify.positions() + 24*i + 16, 'double') - systemSizeHalfVec.z;
+    for ( i = 0; i < Atomify.numberOfAtoms(); i ++ ) {
+        var x = getValue(Atomify.positions() + 24*i, 'double') - systemSizeHalfVec.x;
+        var y = getValue(Atomify.positions() + 24*i + 8, 'double') - systemSizeHalfVec.y;
+        var z = getValue(Atomify.positions() + 24*i + 16, 'double') - systemSizeHalfVec.z;
 
-		var radius = 0.1;
-		position[12*i+0] = x;
+        var radius = 0.1;
+        position[12*i+0] = x;
         position[12*i+1] = y;
         position[12*i+2] = z;
 
@@ -124,29 +125,29 @@ function updateVertices() {
         indexBuffer[6*i+3] = 4*i+0;
         indexBuffer[6*i+4] = 4*i+1;
         indexBuffer[6*i+5] = 4*i+3;
-	}
-	
-	geometry.getAttribute("position").needsUpdate = true
+    }
+    
+    geometry.getAttribute("position").needsUpdate = true
     geometry.getAttribute("vertexId").needsUpdate = true
     geometry.getAttribute("radius").needsUpdate = true
 }
 
 function animate() {
-	if(!Atomify.active()) {
-		return;
-	}
-	Atomify.runCommands("run 1 pre no post no");
-	animationId = requestAnimationFrame( animate );
-	controls.update();
-	updateVertices();
-	render();
+    if(!Atomify.active()) {
+        return;
+    }
+    Atomify.runCommands("run 1 pre no post no");
+    animationId = requestAnimationFrame( animate );
+    controls.update();
+    updateVertices();
+    render();
 }
 
 function render() {
-	t = (Date.now() - t0) / 1000;
-	mesh.updateMatrix();
+    t = (Date.now() - t0) / 1000;
+    mesh.updateMatrix();
     camera.updateMatrixWorld();
-	renderer.render( scene, camera );
+    renderer.render( scene, camera );
 }
 
 if ( ! Detector.webgl ) Detector.addGetWebGLMessage();
